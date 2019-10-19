533d7062dbcff8407b0a53d1b4756ccb6a9e6dae
Amber Flina <erzz197+git@gmail.com>
2019-10-19 16:08:58 -0300

    Importing needed function

--- commands/randomTile.js
+++ commands/randomTile.js
@@ -1,4 +1,5 @@
 const emoji = require("../utils/emoji");
+const createTilePool = require("../utils/createTilePool");
 const responses = ["Here's a tile: ", "Does this one work? ", "How about this one? ", "Don't blame me if it's bad: "]
 
 module.exports = message => {
