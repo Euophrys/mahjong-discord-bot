dff03217903469fcf44195f7415ee85d677277ce
Amber <erzz197+git@gmail.com>
2020-03-10 09:52:38 -0300

    Let's see if this works...

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,11 @@
+var schedule = require('node-schedule');
+
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  var date = new Date(2020, 2, 10, 22, 0, 0, 0);
+
+  var j = schedule.scheduleJob(date, function(){
+    client.channels.get("629737480803057685").send("I was scheduled to send this at 10 PM March 3rd. Let's hope it worked!");
+  }).bind(null, client);
 };
