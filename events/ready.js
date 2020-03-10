6f7197cb5c658eec54e912a1523740be9bf61db4
Amber <erzz197+git@gmail.com>
2020-03-10 09:33:17 -0300

    Not quite what I wanted

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.channels.get("629737480803057685").send("The current time for me is " + Date.now().toString());
+  let date = new Date();
+  client.channels.get("629737480803057685").send("The current time for me is " + date.toString() + " / " + date.toISOString());
 };
