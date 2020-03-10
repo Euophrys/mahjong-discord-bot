8645fb74e214d7b7469c0d1087f81e0ac45e1dd6
Amber <erzz197+git@gmail.com>
2020-03-10 09:40:57 -0300

    Testing finished

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,3 @@
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}!`);
-  let date = new Date();
-  client.channels.get("629737480803057685").send("The current time for me is " + date.toString() + " / " + date.toISOString());
 };
