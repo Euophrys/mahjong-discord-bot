dbb3a87eb88384eafb6750b3fdda491e8dbc72e4
Amber <erzz197+git@gmail.com>
2020-03-10 09:29:52 -0300

    Checking what timezone she uses

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.channels.get("629737480803057685").send("The current time for me is " + Date.now().toString());
 };
