15086df3a3b9f6d14d19a5f4fbe9ef5d0a7a1f79
Amber <erzz197+git@gmail.com>
2020-03-11 16:16:00 -0300

    Cool it works

--- events/ready.js
+++ events/ready.js
@@ -9,19 +9,6 @@ module.exports = client => {
 
   for (let i = 0; i < games.length; i++) {
     var date = new Date(2020, games[i].month, games[i].day, games[i].hour, 0, 0, 0);
-    
-    if (i === 0) {
-      let game = games[i];
-      var winner1 = game.teams[Math.floor(Math.random() * game.teams.length)];
-      var winner2 = game.teams[Math.floor(Math.random() * game.teams.length)];
-      var teamsString = game.teams.slice(0, -1).join(", ") + ', and ' + game.teams.slice(-1);
-      var message = `Day ${i + 1} of the M-League semi-finals is now live! Playing today is ${teamsString}. Come watch at <https://abema.tv/now-on-air/mahjong> (or catch the VoD afterwards)! My money is on ${winner1} for the first hanchan, and ${winner2} for the second.`;
-      client.channels.get("629737480803057685").send(message).then((msg) => {
-        for(let i = 0; i < game.teams.length; i++) {
-          msg.react(mleague.teams[game.teams[i]].emoji);
-        }
-      });
-    }
 
     if (date < now) continue; // game already happened
 
@@ -29,7 +16,7 @@ module.exports = client => {
       var winner1 = game.teams[Math.floor(Math.random() * game.teams.length)];
       var winner2 = game.teams[Math.floor(Math.random() * game.teams.length)];
       var teamsString = game.teams.slice(0, -1).join(", ") + ', and ' + game.teams.slice(-1);
-      var message = `Day ${i + 1} of the M-League semi-finals is now live! Playing today is ${teamsString}. Come watch at <https://abema.tv/now-on-air/mahjong> (or catch the VoD afterwards)! My money is on ${winner1} for the first hanchan, and ${winner2} for the second.`;
+      var message = `Day ${i + 1} of the M-League semi-finals is now live! Playing today is ${teamsString}. Come watch at <https://abema.tv/now-on-air/mahjong> (or catch the VoD afterwards)! My money is on ${winner1} for the first hanchan, and ${winner2} for the second. React with the team you think will win!`;
       client.channels.get("669652926037360651").send(message).then((msg) => {
         for(let i = 0; i < game.teams.length; i++) {
           msg.react(mleague.teams[game.teams[i]].emoji);
