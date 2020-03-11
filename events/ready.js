69573d9dd28fc74f9c4e0bd2e0f4ff6866e77669
Amber <erzz197+git@gmail.com>
2020-03-11 16:12:48 -0300

    Fixing references

--- events/ready.js
+++ events/ready.js
@@ -11,10 +11,11 @@ module.exports = client => {
     var date = new Date(2020, games[i].month, games[i].day, games[i].hour, 0, 0, 0);
     
     if (i === 0) {
-      var winner1 = games[i].teams[Math.floor(Math.random() * games[i].teams.length)];
-      var winner2 = games[i].teams[Math.floor(Math.random() * games[i].teams.length)];
-      var teamsString = games[i].teams.slice(0, -1).join(", ") + ', and ' + games[i].teams.slice(-1);
-      var message = `THIS IS A TEST. Day ${i + 1} of the M-League semi-finals is now live! Playing today is ${teamsString}. Come watch at <https://abema.tv/now-on-air/mahjong> (or catch the VoD afterwards)! My money is on ${winner1} for the first hanchan, and ${winner2} for the second.`;
+      let game = games[i];
+      var winner1 = game.teams[Math.floor(Math.random() * game.teams.length)];
+      var winner2 = game.teams[Math.floor(Math.random() * game.teams.length)];
+      var teamsString = game.teams.slice(0, -1).join(", ") + ', and ' + game.teams.slice(-1);
+      var message = `Day ${i + 1} of the M-League semi-finals is now live! Playing today is ${teamsString}. Come watch at <https://abema.tv/now-on-air/mahjong> (or catch the VoD afterwards)! My money is on ${winner1} for the first hanchan, and ${winner2} for the second.`;
       client.channels.get("629737480803057685").send(message).then((msg) => {
         for(let i = 0; i < game.teams.length; i++) {
           msg.react(mleague.teams[game.teams[i]].emoji);
@@ -25,9 +26,9 @@ module.exports = client => {
     if (date < now) continue; // game already happened
 
     var job = schedule.scheduleJob(date, function(client, game){
-      var winner1 = games[i].teams[Math.floor(Math.random() * games[i].teams.length)];
-      var winner2 = games[i].teams[Math.floor(Math.random() * games[i].teams.length)];
-      var teamsString = games[i].teams.slice(0, -1).join(", ") + ', and ' + games[i].teams.slice(-1);
+      var winner1 = game.teams[Math.floor(Math.random() * game.teams.length)];
+      var winner2 = game.teams[Math.floor(Math.random() * game.teams.length)];
+      var teamsString = game.teams.slice(0, -1).join(", ") + ', and ' + game.teams.slice(-1);
       var message = `Day ${i + 1} of the M-League semi-finals is now live! Playing today is ${teamsString}. Come watch at <https://abema.tv/now-on-air/mahjong> (or catch the VoD afterwards)! My money is on ${winner1} for the first hanchan, and ${winner2} for the second.`;
       client.channels.get("669652926037360651").send(message).then((msg) => {
         for(let i = 0; i < game.teams.length; i++) {
