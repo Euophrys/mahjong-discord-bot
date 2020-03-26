4ebd76402b83c17848e3db0b68748365090cf135
Amber Flina <erzz197+git@gmail.com>
2020-03-26 09:47:03 -0300

    Removing space from Sega Sammy

--- utils/mleague.js
+++ utils/mleague.js
@@ -5,13 +5,13 @@ module.exports = {
         {month: 2, day: 19, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Shibuya Abemas", "U-Next Pirates"]},
         {month: 2, day: 20, hour: 19, teams: ["Konami MFC", "Shibuya Abemas", "SegaSammy Phoenix", "Raiden"]},
         {month: 2, day: 21, hour: 17, teams: ["Sakura Knights", "Shibuya Abemas", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 23, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Sega Sammy Phoenix", "U-Next Pirates"]},
+        {month: 2, day: 23, hour: 19, teams: ["Sakura Knights", "Konami MFC", "SegaSammy Phoenix", "U-Next Pirates"]},
         {month: 2, day: 24, hour: 19, teams: ["Konami MFC", "Shibuya Abemas", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 26, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Sega Sammy Phoenix", "Raiden"]},
-        {month: 2, day: 27, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "Sega Sammy Phoenix", "U-Next Pirates"]},
+        {month: 2, day: 26, hour: 19, teams: ["Sakura Knights", "Konami MFC", "SegaSammy Phoenix", "Raiden"]},
+        {month: 2, day: 27, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "U-Next Pirates"]},
         {month: 2, day: 29, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 30, hour: 19, teams: ["Shibuya Abemas", "Sega Sammy Phoenix", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 31, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Shibuya Abemas", "Sega Sammy Phoenix"]}
+        {month: 2, day: 30, hour: 19, teams: ["Shibuya Abemas", "SegaSammy Phoenix", "Raiden", "U-Next Pirates"]},
+        {month: 2, day: 31, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Shibuya Abemas", "SegaSammy Phoenix"]}
     ],
     teams: {
         "Sakura Knights": { emoji: "686937993377742899" },
