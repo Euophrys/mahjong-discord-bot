c2fcffff7020a152a0ef5a60fe64b4b30f1f6aac
Amber Flina <erzz197+git@gmail.com>
2020-03-31 19:51:34 -0300

    Updated m-league to the finals.

--- utils/mleague.js
+++ utils/mleague.js
@@ -1,17 +1,11 @@
 module.exports = { 
     games: [
-        {month: 2, day: 16, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "Raiden"]},
-        {month: 2, day: 17, hour: 19, teams: ["Konami MFC", "SegaSammy Phoenix", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 19, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Shibuya Abemas", "U-Next Pirates"]},
-        {month: 2, day: 20, hour: 19, teams: ["Konami MFC", "Shibuya Abemas", "SegaSammy Phoenix", "Raiden"]},
-        {month: 2, day: 21, hour: 17, teams: ["Sakura Knights", "Shibuya Abemas", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 23, hour: 19, teams: ["Sakura Knights", "Konami MFC", "SegaSammy Phoenix", "U-Next Pirates"]},
-        {month: 2, day: 24, hour: 19, teams: ["Konami MFC", "Shibuya Abemas", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 26, hour: 19, teams: ["Sakura Knights", "Konami MFC", "SegaSammy Phoenix", "Raiden"]},
-        {month: 2, day: 27, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "U-Next Pirates"]},
-        {month: 2, day: 29, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 30, hour: 19, teams: ["Shibuya Abemas", "SegaSammy Phoenix", "Raiden", "U-Next Pirates"]},
-        {month: 2, day: 31, hour: 19, teams: ["Sakura Knights", "Konami MFC", "Shibuya Abemas", "SegaSammy Phoenix"]}
+        {month: 3, day: 13, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "U-Next Pirates"]},
+        {month: 3, day: 14, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "U-Next Pirates"]},
+        {month: 3, day: 16, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "U-Next Pirates"]},
+        {month: 3, day: 17, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "U-Next Pirates"]},
+        {month: 3, day: 20, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "U-Next Pirates"]},
+        {month: 3, day: 21, hour: 19, teams: ["Sakura Knights", "Shibuya Abemas", "SegaSammy Phoenix", "U-Next Pirates"]}
     ],
     teams: {
         "Sakura Knights": { emoji: "686937993377742899" },
