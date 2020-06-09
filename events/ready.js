d232ebe6f9143d72e083fc065a44e8cd432d7c97
Amber Flina <erzz197+git@gmail.com>
2020-06-09 10:00:46 -0300

    Docs lied

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const mleague = require('../utils/mleague');
 
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log(`Present in ${client.guilds}`);
+  console.log(`Present in ${client.guilds.size} servers.`);
   
   let now = new Date();
   let games = mleague.games;
