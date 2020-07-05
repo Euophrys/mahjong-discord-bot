08f21e5fe3deceaeeb1711ec331fc3129835cf2b
Amber Flina <erzz197+git@gmail.com>
2020-07-05 11:15:59 -0300

    Let's try to make a graph.

--- commands/graph.js
+++ commands/graph.js
@@ -5,7 +5,7 @@ const tenhouRegex = /\/\?log=(.+?)&tw/;
 
 module.exports = (message, client) => {
     let match = tenhouRegex.exec(message.content);
-
+    
     if (!match) {
         return sendResponse(message, "This command requires a full Tenhou replay URL. For example, <https://tenhou.net/0/?log=2020070514gm-00a9-0000-e65fb7ff&tw=1>");
     }
@@ -18,13 +18,54 @@ module.exports = (message, client) => {
         },
         timeout: 10000
     }
-
+    
     request(options, (err, res, body) => {
         if(err) {
             return sendDeletableResponse(message, `${err}`);
         }
+        
+        let graphData = {
+            type: 'line',
+            data: {
+                labels: [],
+                datasets: [{
+                    label: 'A',
+                    data: [ ]
+                }, {
+                    label: 'B',
+                    data: [ ]
+                }, {
+                    label: 'C',
+                    data: [ ]
+                }, {
+                    label: 'D',
+                    data: [ ]
+                }]
+            }
+        }
+        
+        let matches = 0;
+        let match;
+        const endRegex = /[AGARI|RYUUKYOKU].+?sc="(.+?)"/g
+        while(match = endRegex.exec(body)) {
+            matches++;
+            graphData.data.labels.push(matches);
 
-        return sendResponse(message, body);
+            let scores = match.split(",");
+
+            for (let i = 0; i < scores.length; i += 2) {
+                score = parseInt(scores[i]) * 100;
+                change = parseInt(scores[i + 1]) * 100;
+                score += change;
+                graphData.data.datasets[i / 2].data.push(score);
+            }
+        }
+
+        if (matches == 0) {
+            return sendDeletableResponse("Hm, something went wrong. Was that link really a replay?")
+        }
+
+        return sendResponse(message, `https://quickchart.io/chart?c=${JSON.stringify(graphData)}`);
     });
 
     return sendResponse(message, content);
