ad0f042b6203aa36df92017f3edfacf8629f3f32
kator-278 <kator_278@126.com>
2020-09-22 00:19:09 +0800

    Updated hime link; Added Janryumon M.
    1. Hime link is updated, as the client branch of bilibili is less active than others. Either steam's link or JP link is OK.
    2. Easy introduction is provided, but seemly not quite decent. Further wording improvement may be required.

--- commands/platforms.js
+++ commands/platforms.js
@@ -21,7 +21,8 @@ const platforms = {
     "ffxiv": "Final Fantasy XIV has a Mahjong minigame found inside the Gold Saucer, with rank and matchmaking. As it is an MMO, there is a lot of character customization to be had. To access this, start a free trial and follow the story until completing the airships quest (around level 15 or so). Then, in Ul'dah, you'll find a quest called 'It Could Happen To You' which will unlock the Gold Saucer, and therefore Mahjong. https://www.finalfantasyxiv.com/",
     "kemono": "Kemono Mahjong is a paid phone app with a fairly in-depth tutorial. You can play against many AIs with different playstyles, and multiplayer is said to be coming. http://cyberdog.ca/kemono-mahjong/",
     "sega": "Sega Mahjong is a Japanese client. It's primarily for tonpu or sanma play. One notable thing is that, at higher ranks, you have 1.6s to make each move, with a 30s time bank. A very blistering pace. http://www.sega-mj.com/arcade/ Guide: http://arcturus.su/wiki/Sega_MJ",
-    "hime": "Mahjong Hime, or HimeMJ, is a Chinese client with a Japanese translation. An English translation is supposedly in the works. It's very similar to Mahjong Soul, aesthetically, and has a sort of battle pass system. https://game.bilibili.com/qjmj/"
+    "hime": "Mahjong Hime, or HimeMJ, is a Chinese client with a Japanese translation. An English translation is supposedly in the works. It's very similar to Mahjong Soul, aesthetically, and has a sort of battle pass system. https://www.himemj.jp",
+    "janryumonm": "Janryumon M is a mobile platform. The game provides fancy ingame effects, as well as distinct musics. Although with payment, players can still play freely with coins from daily and rewards, as well as free room is also provided. https://jmm.ncsoft.jp",
 };
 
 const aliases = {
@@ -29,5 +30,6 @@ const aliases = {
     "final": "ffxiv",
     "segamj": "sega",
     "himemj": "hime",
-    "janhime": "hime"
+    "janhime": "hime",
+    "jrmm": "janryumonm",
 }
