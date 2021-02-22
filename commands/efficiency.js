7bc560a16545c1739c88a10683a4e6ff35415e59
Amber Flina <erzz197+git@gmail.com>
2021-02-22 10:57:57 -0400

    oh boy here I go debugging again

--- commands/efficiency.js
+++ commands/efficiency.js
@@ -347,6 +347,11 @@ function calculateUkeireUpgrades(hand, remainingTiles, shantenFunction, baseShan
             let bestUkeire = Math.max(...discards);
             let bestDiscard = discards.indexOf(bestUkeire);
 
+            console.log(addedTile);
+            console.log(discards);
+            console.log(bestUkeire);
+            console.log(bestDiscard);
+
             if (addedTile !== bestDiscard) {
                 // Check the ukeire of the hand after cutting the best tile
                 hand[bestDiscard]--;
