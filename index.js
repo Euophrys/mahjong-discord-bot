4e15dc08b0fc0f5cd390994c230aedba163ed389
Amber Flina <erzz197+git@gmail.com>
2021-08-09 15:27:27 -0300

    debug time

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const fs = require("fs");
 const client = new Discord.Client({intents:[Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES], partials: ['MESSAGE', 'CHANNEL']});
 
 fs.readdir("./events/", (err, files) => {
-  files.forEach(file => {
-    const eventHandler = require(`./events/${file}`);
-    const eventName = file.split(".")[0];
-    client.on(eventName, async (...args) => eventHandler(client, ...args));
-  });
+    files.forEach(file => {
+        const eventHandler = require(`./events/${file}`);
+        const eventName = file.split(".")[0];
+        console.log(eventName)
+        client.on(eventName, async (...args) => {
+            console.log("message?")
+            eventHandler(client, ...args)
+        });
+    });
 });
 
 client.login(process.env.BOT_TOKEN);
