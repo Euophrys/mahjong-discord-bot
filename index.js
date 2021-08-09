51096a6cc2e97d4c426f112d7d759e92aeaa899a
Amber Flina <erzz197+git@gmail.com>
2021-08-09 15:38:48 -0300

    how about doing it manually

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const fs = require("fs");
-const client = new Discord.Client({intents:[Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES], partials: ['MESSAGE', 'CHANNEL']});
 
-fs.readdir("./events/", (err, files) => {
-    files.forEach(file => {
-        const eventHandler = require(`./events/${file}`);
-        const eventName = file.split(".")[0];
-        console.log(eventName)
-        client.on(eventName, async (...args) => {
-            console.log("message?")
-            eventHandler(client, ...args)
-        });
-    });
+const client = new Discord.Client({
+    intents:[Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES],
+    partials: ['MESSAGE', 'CHANNEL']
 });
+
+const messageCreate = require(`./events/message.js`);
+const interactionCreate = require(`./events/interactionCreate.js`);
+const ready = require(`./events/ready.js`);
+
+client.on('messageCreate', (client, message) => {
+    console.log("message");
+    messageCreate(client, message);
+});
+
+client.on('interactionCreate', async interaction => interactionCreate(interaction));
+
+client.on('ready', client => ready(client));
 
 client.login(process.env.BOT_TOKEN);
